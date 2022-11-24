83c170159b38025e8e6a648c081b6e346667b420
Denis Toryansky <dtoryansky@gmail.com>
2022-11-24 18:21:07 +0300

    Task 4 was completed.

---
+++ Seminar_1/Task_4.py
@@ -0,0 +1,15 @@
+# Задача 4.Напишите программу, которая по заданному номеру четверти,
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+coordinate_quarter = int(input("Введите номер четверти: "))
+
+if coordinate_quarter == 1:
+    print("В этой четверти x > 0 и y > 0")
+elif coordinate_quarter == 2:
+    print("В этой четверти x < 0 и y > 0")
+elif coordinate_quarter == 3:
+    print("В этой четверти x < 0 и y < 0")
+elif coordinate_quarter == 4:
+    print("В этой четверти x > 0 и y < 0")
+else:
+    print("Некорректно введён номер")
