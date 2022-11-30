5f4f8dba10a3e34d22dd743b88d97230fce7b456
Denis Toryansky <dtoryansky@gmail.com>
2022-11-30 20:00:52 +0300

    Task 4 was completed

--- Seminar_3/Task_4.py
+++ Seminar_3/Task_4.py
@@ -2,4 +2,12 @@
 # число в двоичное (встроенными методами пользоваться нельзя).
 
 number = int(input("Введите десятичное число для преобразования: "))
-
+sp = []
+if number > 0:
+    while number != 0:
+        binary_digit_value = number % 2
+        sp.append(str(binary_digit_value))
+        number = number // 2
+    print(f"Десятичная форма: " + " ".join(sp[::-1]))
+else:
+    print(f"Десятичная форма: 0")
