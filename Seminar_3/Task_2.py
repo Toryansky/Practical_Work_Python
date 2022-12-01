1d4e1d4d54dda548cc7e8b06d3a2c0833d16cfea
Denis Toryansky <dtoryansky@gmail.com>
2022-12-01 20:51:44 +0300

    Task 2 little improvement

--- Seminar_3/Task_2.py
+++ Seminar_3/Task_2.py
@@ -14,11 +14,11 @@ print(f"Ваш список: {sp}")
 sp2 = []
 if n % 2 == 0:
     for j in range(n//2):
-        x = sp[j] * sp[n-1-j]
-        sp2.append(x)
+        element = sp[j] * sp[n-1-j]
+        sp2.append(element)
     print(f"Произведение пар чисел списка: {sp2}")
 else:
-    for j in range(n//2+1):
-        x = sp[j] * sp[n-1-j]
-        sp2.append(x)
+    for k in range(n//2+1):
+        element = sp[k] * sp[n-1-k]
+        sp2.append(element)
     print(f"Произведение пар чисел списка: {sp2}")
